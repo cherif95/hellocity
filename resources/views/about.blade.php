5eeff4bde815c02bb6bead53ec88622b2078e61c
cherif95 <ouattaraabdoulcherif@gmail.com>
2020-10-26 12:45:45 +0100

    add about page and update links

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,13 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hello city</title>
+        <title>About us | hello citygit</title>
 
     </head>
     <body>
-        <h1>Hello Ivory coast</h1>
-        <p>It's currently {{date('h:i A')}}</p>
-
+        <p>Build with &hearts; by me</p>
+        <p><a href="/">Revenir à la page d'accueil</a></p>
         <footer>
             <p>&copy; Copyright {{date('Y')}} &middot; <a href="/about-us">About us</a></p>
         </footer>
