4cfdf4ce2f260907e1da9792dab48be4ce37afaa
cherif95 <ouattaraabdoulcherif@gmail.com>
2020-11-04 13:21:52 +0100

    add images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,6 +3,7 @@
 @section('title','About Us | '.config('app.name'))
 
 @section('content')
+        <img src="/images/guep.jpg" alt="guep">
         <p>Build with &hearts; by me</p>
         <p><a href="{{route('home')}}">Revenir à la page d'accueil</a></p>
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,6 +3,7 @@
 @section('title',config('app.name'))
 
 @section('content')   
+        <img src="/images/ci.jpg" alt="ci flag">
         <h1>Hello Ivory coast</h1>
         <p>It's currently {{date('h:i A')}}</p>
 @endsection
